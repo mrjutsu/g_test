7cf764890d90373cdb608457f120d5ccf528e4a5
David Rosillo <drr538@gmail.com>
2016-04-22 19:05:22 -0500

    Added Registrations Controller to modify a user's info

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  
+
   resources :messages, only: :create
   resources :categories
   resources :groups
 
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
 
   devise_scope :user do
     get :sign_up, to: 'devise/registrations#new'
