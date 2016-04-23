0e73462d18195d473a455f4a920330b658167e63
David Rosillo <drr538@gmail.com>
2016-04-23 11:26:11 -0500

    Succesfully create public and private messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -9,7 +9,7 @@ class Message < ApplicationRecord
   validates :body, presence: { message: 'El mensaje no puede estar vacío.' }
   # validates :messages_categories, presence: { message: 'El mensaje necesita al menos una categoría.' }
 
-  scope :public_messages, -> (id) { where(privacy: 0).or( Message.where( privacy: 1, user_id: id ) ).order(created_at: :asc) }
+  scope :public_messages, -> (id) { where(privacy: 0).or( Message.where( privacy: 1, user_id: id ) ).order(created_at: :desc) }
 
   scope :visible_messages, -> (id,ids) { self.public_messages(id) + Message.find( GroupsMessage.groups_messages(ids) ) }
 
