8945e963c54bb7cff442be561e76a50d0902a6ef
David Rosillo <drr538@gmail.com>
2016-04-23 11:12:43 -0500

    Show categories

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,11 @@
 module CategoriesHelper
+  def count_messages category
+    if category.messages.empty?
+      'No hay mensajes'
+    elsif category.messages == 1
+      '1 mensaje'
+    else
+      category.messages.count.to_s + ' mensajes'
+    end
+  end
 end
--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,2 +1,11 @@
 module GroupsHelper
+  def count_users group
+    if group.participants.empty?
+      'Grupo vacío'
+    elsif group.participants == 1
+      '1 persona'
+    else
+      group.participants.count.to_s + ' personas'
+    end
+  end
 end
