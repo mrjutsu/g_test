e68785a1b75d5446cc42934dc6c1d7732eed7030
David Rosillo <drr538@gmail.com>
2016-04-23 20:06:42 -0500

    Show groups where a message was posted. Changed nav to show name. Changed profile show to solve when users didn't have their full info

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,17 @@
 module ApplicationHelper
+
   def nav
     if user_signed_in?
       render partial: 'layouts/nav'
     end
   end
+
+  def nav_name
+    if current_user.name.nil?
+      'Bienvenido/a'
+    else
+      current_user.name
+    end
+  end
+
 end
--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,2 +1,15 @@
 module MessagesHelper
+
+  def posted_to message
+    grupos = []
+    if message.groups.empty?
+      grupos
+    else
+      current_user.joined_groups.each do |g|
+        message.groups.include? g ? grupos.push(g) : ''
+      end
+      grupos
+    end
+  end
+
 end
