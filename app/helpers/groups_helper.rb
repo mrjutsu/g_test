a1b19a89cbc93a15ba7c5b7d82850b68b8c34500
David Rosillo <drr538@gmail.com>
2016-04-25 18:51:30 -0500

    Added styles for groups and categories

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -22,9 +22,9 @@ module GroupsHelper
 
   def join_leave_group group
     if current_user.joined_groups.include? group
-      link_to 'Salir Del Grupo', leave_group_path( group.slug ), method: :delete
+      link_to 'Salir Del Grupo', leave_group_path( group.slug ), method: :delete, class: "btn btn-danger bot-btn"
     else
-      link_to 'Unirse Al Grupo', join_group_path( group.slug ), method: :post
+      link_to 'Unirse Al Grupo', join_group_path( group.slug ), method: :post, class: "btn btn-success bot-btn"
     end
   end
 
