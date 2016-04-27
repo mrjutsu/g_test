9eae731674ed1805572ede360e3d9420f9f19ca1
David Rosillo <drr538@gmail.com>
2016-04-27 12:10:01 -0500

    Deleted unnecessary stuff

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,63 +2,46 @@ class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy, :set_messages]
   before_action :set_messages, only: :show
 
-  # GET /categories
-  # GET /categories.json
   def index
     @categories = Category.all.order(:name)
   end
 
-  # GET /categories/1
-  # GET /categories/1.json
   def show
   end
 
-  # GET /categories/new
   def new
     @category = Category.new
   end
 
-  # GET /categories/1/edit
   def edit
   end
 
-  # POST /categories
-  # POST /categories.json
   def create
     @category = Category.new(category_params)
 
     respond_to do |format|
       if @category.save
         format.html { redirect_to category_path(@category.slug), notice: 'Categoría creada exitosamente' }
-        format.json { render :show, status: :created, location: @category }
       else
         format.html { render :new }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /categories/1
-  # PATCH/PUT /categories/1.json
   def update
     respond_to do |format|
       if @category.update(category_params)
         format.html { redirect_to @category, notice: 'Categoría editada exitosamente' }
-        format.json { render :show, status: :ok, location: @category }
       else
         format.html { render :edit }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /categories/1
-  # DELETE /categories/1.json
   def destroy
     @category.destroy
     respond_to do |format|
       format.html { redirect_to categories_url, notice: 'Categoría destruida' }
-      format.json { head :no_content }
     end
   end
 
--- app/views/messages/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @message, :id, :body, :privacy, :user_id, :created_at, :updated_at
